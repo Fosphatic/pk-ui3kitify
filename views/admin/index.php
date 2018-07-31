3d81222a9779f74b6a8919fd9c3fb320f1840358
Fosphatic-Duke <f.duke@web.de>
2018-07-31 22:05:29 +0200

    Initial Release 1.0.0

--- views/admin/index.php
+++ views/admin/index.php
@@ -2,15 +2,15 @@
   <div class="uk-grid uk-margin-large-bottom">
     <div class="uk-width-medium-2-3 uk-flex uk-flex-middle uk-flex-center">
       <div class="uk-text-center">
-        <h1>Framework</h1>
-        <p class="uk-text-large">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vel posuere erat, et mollis libero. Sed ut sem augue. Nam rhoncus dignissim quam a pharetra.</p>
+        <h1>UI3Kitify</h1>
+        <p class="uk-text-large">UI3Kitify is a new template based on pastheme work for Pagekit using the new UIKit v3 RC10 Framework.</p>
         <div class="uk-flex uk-flex-center">
           <ul class="uk-grid uk-text-large">
             <li>
-              <a href="https://github.com/Pastheme/pas-framework" target="_blank"><i class="uk-icon-github"></i> Github</a>
+              <a href="https://github.com/Fosphatic/pk-ui3kitify" target="_blank"><i class="uk-icon-github"></i> Github</a>
             </li>
             <li>
-              <a href="https://pastheme.com/framework" target="_blank"><i class="uk-icon-book"></i> Project Page</a>
+              <a href="https://getuikit.com/" target="_blank"><i class="uk-icon-book"></i> UIkit Framework</a>
             </li>
           </ul>
         </div>
@@ -25,7 +25,7 @@
     <div class="uk-width-medium-1-3 uk-flex uk-flex-middle uk-flex-center">
       <div class="uk-text-center">
         <p class="uk-text-large">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vel posuere erat, et mollis libero. Sed ut sem augue. Nam rhoncus dignissim quam a pharetra.
+          It is a fancy theme that is easy to use and customize. There is a demo available too, if you have some questions don't hasistate to open a thread in the pagekit forum!
         </p>
       </div>
     </div>
